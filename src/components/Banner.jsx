289043deef656b083dd3f64e58228def30139629
muratcansalihW <muratcansalihwork@hotmail.com>
2025-01-03 00:44:05 +0300

    test

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import video from "../assets/banner_video.mp4";
 
 function Banner() {
   return (
-    <div>
-      <h1>GET JOB</h1>
-    </div>
+    <section className="bg-hero-pattern min-h-screen">
+      <video className="videoTag" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="container m-auto">
+        <div className="justify-around min-h-screen flex items-center ">
+          <div className="grid-rows-6">
+            <h1 className="text-9xl text-aqua drop-shadow-xl">GET JOB</h1>
+          </div>
+          <div className="grid-rows-6"></div>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('./assets/bg_banner.png')",
+      },
+      colors: {
+        aqua: "#81D8D0",
+      },
+    },
   },
   plugins: [],
 };
