96af7f3311498ade744fdc11754615c25bf69da3
farukozgu <faruk.ozgufb@gmail.com>
2025-01-03 01:00:41 +0300

    tailwind config fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-pattern": "url('./assets/bg_banner.png')",
+      },
+      colors: {
+        aqua: "#81D8D0",
+      },
+    },
   },
   plugins: [],
 };
