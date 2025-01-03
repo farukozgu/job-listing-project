c98cdf9cfa15746f1ec8242df8c7e787a2ed9133
muratcansalihW <muratcansalihwork@hotmail.com>
2025-01-03 22:44:44 +0300

    footerUpButton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-
+import Arrow from "../assets/up-arrow.png";
+const ScrollToTop = () => {};
+const handleScrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Yumuşak kaydırma efekti
+  });
+};
 function Footer() {
   return (
     <div>
-      <section className="footer">
-        <div className="container m-auto min-h-48 justify-center items-center flex">
-          <div className="flex justify-center">
-            <div className="grid-cols-12">Powered by Muratcan</div>
+      <section className="footer bg-gray-900 text-white py-6">
+        <div className="container mx-auto grid grid-cols-12 items-center">
+          <div className="col-span-6 text-center sm:text-left">
+            <p className="text-sm">Copyright 2025 ©</p>
+          </div>
+          <div className="col-span-6 text-center sm:text-right flex justify-end items-center gap-2">
+            <p className="text-sm">Designed by Muratcan Salih</p>
+            <img
+              src={Arrow}
+              id="upArrow"
+              className="w-full max-w-8"
+              alt="Yukarı Git"
+              onClick={handleScrollToTop}
+            />
           </div>
         </div>
       </section>
