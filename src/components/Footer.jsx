801f86102fd5cd92d3ad0109107d8f0ad3b3bb44
muratcansalihW <muratcansalihwork@hotmail.com>
2025-01-03 22:51:32 +0300

    footerUpdate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import Arrow from "../assets/up-arrow.png";
-const ScrollToTop = () => {};
+
 const handleScrollToTop = () => {
   window.scrollTo({
     top: 0,
-    behavior: "smooth", // Yumuşak kaydırma efekti
+    behavior: "smooth",
   });
 };
 function Footer() {
   return (
     <div>
-      <section className="footer bg-gray-900 text-white py-6">
+      <section className="footer bg-blue-600 text-white py-6">
         <div className="container mx-auto grid grid-cols-12 items-center">
-          <div className="col-span-6 text-center sm:text-left">
-            <p className="text-sm">Copyright 2025 ©</p>
+          <div className="col-span-4 text-center sm:text-left">
+            <p className="text-sm font-inter font-medium">Copyright 2025 ©</p>
           </div>
-          <div className="col-span-6 text-center sm:text-right flex justify-end items-center gap-2">
-            <p className="text-sm">Designed by Muratcan Salih</p>
+          <div className="col-span-4 text-center sm:text-center">
+            <h1 className="font-inter font-extrabold text-3xl text-white pr-5">
+              JobList
+            </h1>
+          </div>
+          <div className="col-span-4 text-center sm:text-right flex justify-end items-center gap-2">
+            <p className="text-sm font-inter font-medium">GCMA CODE</p>
             <img
               src={Arrow}
               id="upArrow"
