4de064a0220b3f20edfdc0a8f71649363148110a
muratcansalihW <muratcansalihwork@hotmail.com>
2025-01-04 00:18:10 +0300

    footerMobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,18 @@ function Footer() {
   return (
     <div>
       <section className="footer bg-blue-600 text-white py-6">
-        <div className="container mx-auto grid grid-cols-12 items-center">
-          <div className="col-span-4 text-center sm:text-left">
-            <p className="text-sm font-inter font-medium">Copyright 2025 ©</p>
+        <div className="container mx-auto grid grid-cols-12 items-center gap-5 sm:gap-0">
+          <div className="col-span-12 sm:col-span-4">
+            <p className="text-sm font-inter font-medium text-center sm:text-start">
+              Copyright 2025 ©
+            </p>
           </div>
-          <div className="col-span-4 text-center sm:text-center">
-            <h1 className="font-inter font-extrabold text-3xl text-white pr-5">
+          <div className="col-span-12 sm:col-span-4 text-center sm:text-center">
+            <h1 className="font-inter font-extrabold text-3xl text-white pr-0 sm:pr-5">
               JobList
             </h1>
           </div>
-          <div className="col-span-4 text-center sm:text-right flex justify-end items-center gap-2">
+          <div className="col-span-12 sm:col-span-4 text-center sm:text-center flex justify-center sm:justify-end items-center gap-2">
             <p className="text-sm font-inter font-medium">GCMA CODE</p>
             <img
               src={Arrow}
